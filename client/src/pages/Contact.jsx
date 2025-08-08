d8030f7852674ff7b9a4095e7d66007a2845cdc5
Sajid416 <sajidulislam6183@gmail.com>
2025-08-08 21:53:07 +0600

    visitors auto-reply email and author notifications email feature is added

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,127 +1,179 @@
 import React, { useRef } from "react";
 import img from "../assets/img/contact.jpg";
 import { useForm } from "react-hook-form";
-import emailjs from '@emailjs/browser';
-
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  const form=useRef();
+  const form = useRef();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
 
-  const sendEmail=(e)=>{
+  const sendEmail = (e) => {
     e.preventDefault();
-    emailjs.sendEmail("service_b641t1g","template_ijbip7j",
-      form.current,
-      "zzEyGwvrfoRmCDL9V"
-    )
-    .then(()=>{
-      alert("Email send Successfully")
-      form.current.reset()
-    })
-    .catch((err)=>{
-      console.error(err)
-      alert("Something went wrong")
-    })
-  }
+
+    // Sent Auto-reply email to visitors
+    emailjs
+      .send(
+        "service_b641t1g",
+        "template_ijbip7j", // Auto-reply template ID
+        {
+          to_name: e.target.cName.value,
+          to_email: e.target.email.value,
+          message:
+            "Thank you for reaching out to BlogVerse! We’ve received your message and will get back to you soon.",
+        },
+        "zzEyGwvrfoRmCDL9V"
+      )
+      .then(() => {
+        console.log("Auto-reply sent successfully");
+      })
+      .catch((error) => {
+        console.error("Auto-reply error:", error);
+      });
+
+    // message sent to the Owner
+    emailjs
+      .sendForm(
+        "service_b641t1g",
+        "template_yyf109k", //  Owner notification template ID
+        form.current,
+        "zzEyGwvrfoRmCDL9V"
+      )
+      .then(() => {
+        alert("Message sent successfully!");
+        form.current.reset();
+      })
+      .catch((error) => {
+        console.error("Owner notification error:", error);
+        alert("Failed to send message.");
+      });
+  };
 
   return (
-    <div className="p-10 w-full pl-3">
-      <div className=" w-3/4 mx-auto flex flex-col gap-5">
-        <h1 className="text-center text-4xl font-bold font-pacifico mb-2">
+    <div className="px-4 py-10 w-full">
+      <div className="max-w-6xl mx-auto flex flex-col gap-8">
+        <h1 className="text-center text-3xl sm:text-4xl font-bold font-pacifico">
           Contact Us
         </h1>
-        <div className="flex flex-row gap-6 p-5">
-          <div className="w-[500px] h-[400px]">
-            <img src={img} alt="Contact img" className="w-full h-full rounded-2xl object-cover object-[right]" />
+
+        <div className="flex flex-col lg:flex-row gap-6 p-5">
+          {/* Image Section */}
+          <div className="w-full lg:w-1/2 h-64 sm:h-80 lg:h-[400px] overflow-hidden rounded-2xl">
+            <img
+              src={img}
+              alt="Contact img"
+              className="w-full h-full object-cover object-center"
+            />
           </div>
-          <div>
+
+          {/* Form Section */}
+          <div className="w-full lg:w-1/2">
             <form
+              ref={form}
               onSubmit={sendEmail}
-              className="flex flex-col gap-5 pl-15 w-full max-w-sm"
+              className="flex flex-col gap-5 w-full"
             >
+              {/* Full Name */}
               <div className="flex flex-col gap-2">
                 <label
                   htmlFor="cName"
-                  className="block text-gray-700 text-sm font-medium "
+                  className="text-gray-700 text-sm font-medium"
                 >
-                  FullName
+                  Full Name
                 </label>
                 <input
                   type="text"
                   id="cName"
-                  {...register("cName",{required:true})}
-                  aria-invalid={errors.cName? "true":"false"}
+                  {...register("cName", { required: true })}
+                  aria-invalid={errors.cName ? "true" : "false"}
+                  placeholder="Enter your name"
                   name="cName"
-                  placeholder="Enter Your Name"
-                  className="px-4 py-2 border border-gray-300 font-medium rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                  className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
                 />
-                {errors.cName?.type==="required" && (
-                  <p className="text-red-500 text-sm mt-1" role="alert">FullName is required</p>
+                {errors.cName && (
+                  <p className="text-red-500 text-sm">Full name is required</p>
                 )}
               </div>
+
+              {/* Email */}
               <div className="flex flex-col gap-2">
                 <label
                   htmlFor="email"
-                  className="block text-gray-700 text-sm font-medium "
+                  className="text-gray-700 text-sm font-medium"
                 >
                   Email Address
                 </label>
                 <input
                   type="email"
                   id="email"
-                  {...register("email",{required:true})}
-                  aria-invalid={errors.email? "true":"false"}
+                  {...register("email", { required: true })}
+                  aria-invalid={errors.email ? "true" : "false"}
+                  placeholder="Enter your email address"
                   name="email"
-                  placeholder="Enter Your Email Address"
-                  className="px-4 py-2 border border-gray-300 font-medium rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                  className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
                 />
-                {errors.email?.type==="required" && (
-                  <p className="text-red-500 text-sm mt-1">Email is required</p>
+                {errors.email && (
+                  <p className="text-red-500 text-sm">Email is required</p>
                 )}
               </div>
+
+              {/* Subject */}
               <div className="flex flex-col gap-2">
                 <label
                   htmlFor="subject"
-                  className="block text-gray-700 text-sm font-medium "
+                  className="text-gray-700 text-sm font-medium"
                 >
                   Subject
                 </label>
                 <input
                   type="text"
                   id="subject"
-                  {...register("subject",{required:true})}
-                  aria-invalid={errors.subject? "true":"false"}
+                  {...register("subject", { required: true })}
+                  aria-invalid={errors.subject ? "true" : "false"}
+                  placeholder="Enter subject"
                   name="subject"
-                  placeholder="Enter Your Subject"
-                  className="px-4 py-2 border border-gray-300 font-medium rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                  className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
                 />
-                {errors.subject?.type==="required"
-                  && (
-                    <p className="text-red-500 text-sm mt-1">Subject is required</p>
-                  )
-                }
+                {errors.subject && (
+                  <p className="text-red-500 text-sm">Subject is required</p>
+                )}
               </div>
+
+              {/* Message */}
               <div className="flex flex-col gap-2">
                 <label
                   htmlFor="message"
-                  className="block text-gray-700 text-sm font-medium "
+                  className="text-gray-700 text-sm font-medium"
                 >
                   Message
                 </label>
                 <textarea
                   id="message"
-                  name="message"
-                  {...register("message",{required:true})}
-                  aria-invalid={errors.message? "true":"false"}
+                  {...register("message", { required: true })}
+                  aria-invalid={errors.message ? "true" : "false"}
                   rows="5"
-                  className="w-full border border-gray-300 rounded-lg p-3 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent resize-none shadow-sm"
                   placeholder="Type your message here..."
+                  name="message"
+                  className="w-full border border-gray-300 rounded-lg p-3 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none shadow-sm"
                 ></textarea>
-                {errors.message?.type==="required" &&
-                 <p className="text-red-500 font-sm mt-1">Message field is required</p>
-                }
+                {errors.message && (
+                  <p className="text-red-500 text-sm">
+                    Message field is required
+                  </p>
+                )}
               </div>
+
+              {/* Submit Button */}
               <div>
-                <input type="submit" className="bg-blue-600 px-4 py-2 font-semibold text-white rounded-lg hover:bg-blue-500 cursor-pointer transition" />
+                <input
+                  type="submit"
+                  value="Send"
+                  className="bg-blue-600 px-4 py-2 font-semibold text-white rounded-lg hover:bg-blue-500 cursor-pointer transition"
+                />
               </div>
             </form>
           </div>
