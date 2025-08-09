3ebe3566ef19d36926b7cdb379b983948afcac61
Sajid416 <sajidulislam6183@gmail.com>
2025-08-09 11:22:15 +0600

    login button has been linked with login page

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -1,12 +1,82 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaFacebook } from "react-icons/fa6";
 import { CiTwitter } from "react-icons/ci";
 import { FaWhatsapp } from "react-icons/fa";
+
 import { FaArrowRightLong } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const ProfileDropdown=()=>{
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  // Close dropdown if clicked outside
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  return (
+    <div className="relative inline-block text-left" ref={dropdownRef}>
+      <button
+        onClick={() => setOpen(!open)}
+        className="inline-flex justify-center w-full rounded-md hover:font-bold shadow-sm px-4 py-2 bg-orange-500 text-sm font-medium text-white hover:bg-orange-600 focus:outline-none"
+        aria-expanded={open}
+        aria-haspopup="true"
+      >
+        Profile
+        <svg
+          className="ml-2 -mr-1 h-5 w-5 text-gray-500"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            fillRule="evenodd"
+            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+
+      {open && (
+        <div className="origin-top-right absolute right-0 mt-2 w-44 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
+          <div className="py-1">
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 border-b border-gray-200"
+            >
+              Create Blog
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 border-b border-gray-200"
+            >
+              myBlog
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            >
+              Logout
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+
+const Navbar = () => {  
+  const [user, setUser] = useState(null);
   const list = [
     { item: "Home", url: "/" },
     { item: "Services", url: "services" },
@@ -43,12 +113,20 @@ const Navbar = () => {
             <CiTwitter />
             <FaWhatsapp />
           </div>
-          <button className="bg-orange-500 text-white font-semibold cursor-pointer rounded-md hover:bg-orange-400 transition px-3 py-1">
-            Login
-          </button>
+          {user === null ? (
+            <div>
+              <Link to="login">
+              <button className="bg-orange-500 text-white font-semibold cursor-pointer rounded-md hover:bg-orange-400 transition px-3 py-1">
+                Login
+              </button>
+              </Link>
+              
+            </div>
+          ) : (
+             <ProfileDropdown/>           
+          )}
         </div>
       </div>
-      
     </div>
   );
 };
